05c77540e92f1be84f3ea0d548be0b539eb5b11d
logan1x <sharmakhushal78@gmail.com>
2022-03-22 22:37:53 +0530

    refactor: rewrite html to jsx

--- src/components/nav.js
+++ src/components/nav.js
@@ -19,9 +19,9 @@ export default function nav() {
         >
           {" "}
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           ></path>
         </svg>
       </div>
@@ -37,9 +37,9 @@ export default function nav() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
           <span className="badge">4</span>
