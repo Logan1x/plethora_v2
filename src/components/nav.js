f178183de785fd85c7492a6afd01fc87ea6e6023
logan1x <sharmakhushal78@gmail.com>
2022-08-05 12:40:42 +0530

    [reaf]: added icon for product page, cart styling for uniform image

--- src/components/nav.js
+++ src/components/nav.js
@@ -52,6 +52,22 @@ export default function Nav() {
         </svg>
       </div> */}
       <div className="nav-pills">
+        <Link to="/products">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="nav-badge-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+            />
+          </svg>
+        </Link>
         <Link className="alert-badge" to="/wishlist">
           <svg
             className="nav-badge-icon"
