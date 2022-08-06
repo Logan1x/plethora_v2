98c6d1fd48298aaf939373e3d2a4b07ec7e7a20c
logan1x <sharmakhushal78@gmail.com>
2022-08-06 13:41:03 +0530

    [buggy] : razorpay payment intrigation

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { useDataContext } from "../contexts/dataContext";
+import {useAuth} from "../contexts/authContext";
 import { cartCheckoutNotify } from "../utils/utilCartWishFuncs";
 import notFoundImage from "../assets/undraw_refreshing_beverage_td3r.svg";
 
 export default function Cart() {
   const { state, removeFromCart, increaseItemQuantity, decreaseItemQuantity } =
     useDataContext();
+
+  const { currUser } = useAuth();
 
   const [cartData, setCartData] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
@@ -21,6 +24,58 @@ export default function Cart() {
     }
   };
 
+  const RAZORPAY_URL = "https://checkout.razorpay.com/v1/checkout.js";
+
+  const handleLoadScript = (src) => {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        reject(false);
+      };
+      document.body.appendChild(script);
+    });
+  };
+
+  const handleShowRazorPay = async () => {
+		const response = await handleLoadScript(RAZORPAY_URL);
+
+		if (!response) {
+			cartCheckoutNotify()
+			return;
+		}
+
+		var options = {
+			key: process.env.REACT_APP_RZP_KEY,
+			amount: (Number(totalPrice) - 600) * 100,
+			currency: "INR",
+			name: "Bookery",
+			description: "Thank you for shopping!",
+			image: notFoundImage,
+
+			handler: async function (response) {
+				const { razorpay_payment_id } = await response;
+				// const order = {
+				// 	razorpayPaymentId: razorpay_payment_id,
+				// 	...checkoutData,
+				// };
+				// placeOrder(order);
+			},
+			prefill: {
+				name: `${currUser?.name} ${currUser?.lastName}`,
+				email: currUser?.email,
+				contact: currUser?.phoneNumber,
+			},
+			theme: { color: "#3399cc" },
+		};
+
+		const paymentObject = new Razorpay(options);
+		paymentObject.open();
+	};
+
   useEffect(() => {
     setCartData(getCartData());
     setTotalPrice(getTotalPrice());
@@ -32,7 +87,6 @@ export default function Cart() {
         <div className="container-main">
           <main className="container-pages">
             <h1 className="cart-title-center">Your Cart</h1>
-
             <div className="cart-product-checkout-cards">
               <div className="cart-item-card-parent">
                 {cartData &&
@@ -124,13 +178,13 @@ export default function Cart() {
                 <div className="cart-address-area">
                   <input type="radio" name="" id="" checked />
                   <div className="cart-address-content">
-                    <p className="cart-address-name">Khushal Sharma</p>
+                    <p className="cart-address-name">{`${currUser.firstName} ${currUser.lastName}`}</p>
                     <p className="cart-address-full">15 D Block, Sector 9, Udaipur, 313002</p>
                   </div>
                 </div>
                 </div>
                 <div className="cart-btn cart-checkout">
-                  <button onClick={cartCheckoutNotify}>Checkout</button>
+                  <button onClick={handleShowRazorPay}>Checkout</button>
                 </div>
               </div>
             </div>
